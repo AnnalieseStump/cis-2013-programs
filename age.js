057231695b2e2e1d4f9759da12e00c701d56725e
AnnalieseStump <122641459+AnnalieseStump@users.noreply.github.com>
2023-02-03 14:16:08 -0600

    Add files via upload

--- age.js
+++ age.js
@@ -6,3 +6,4 @@ var floatAge, floatDays, intWeeks, floatMonths, intFortnights;
      intWeeks = parseInt(floatDays / 7).toFixed(2);//calculated weeks based on days alive
      intFortnights = parseInt(floatDays / 14).toFixed(2);//calculated fortnights by dividing days by 14    
      alert("Age = " + floatAge + "\n" + "Weeks = " + floatDays + "\n" + "Days = " + intWeeks + "\n" + "Months = " + floatMonths + "\n" + "Fortnights = " + intFortnights);
+     
